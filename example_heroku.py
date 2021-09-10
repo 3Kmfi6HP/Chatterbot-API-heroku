795bcbb53622b859baf3017de025b2e154a20c61
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2021-09-10 08:45:42 +0700

    chatbot ex heroku

---
+++ example_heroku.py
@@ -0,0 +1,8 @@
+from requests import post as p
+
+username = input('Enter a username: ')
+
+while True:
+    query = input(f'{username}: ')
+    r = p(f'https://mychatbotapp6.herokuapp.com/?query={query}')
+    print('Bot:', r.json()['response']['bot'])
