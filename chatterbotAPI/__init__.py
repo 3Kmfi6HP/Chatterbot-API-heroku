5b8b2c26d7c5a6b6151e2e6c9a7976dc7fdcb10b
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2022-07-16 03:55:55 +0700

    Update __init__.py

--- chatterbotAPI/__init__.py
+++ chatterbotAPI/__init__.py
@@ -41,7 +41,7 @@ trainer.train(
     # Uncomment each line to train from chatterbot's corpus
     # 'chatterbot.corpus.english',
     'chatterbot.corpus.chinese',
-    'chatterbot.corpus.chinese.greetings',
+    #'chatterbot.corpus.chinese.greetings',
     #'chatterbot.corpus.chinese.conversations'
     # 'chatterbot.corpus.spanish',
     # 'chatterbot.corpus.italian'
